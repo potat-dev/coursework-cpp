cc1adfab8aac8cc34934fc11c0be6b546a199ecf
Den <denis100703@gmail.com>
2022-11-09 01:28:35 +0300

    Add more tests

--- test/tests.cpp
+++ test/tests.cpp
@@ -8,12 +8,78 @@
 using namespace std;
 
 TEST_CASE("Numbers Methods", "[number]") {
-  SECTION("Constructor") {
-    Number a("123456789012345678901234567890");
-    Number b("123456789012345678901234567890");
+  SECTION("Constructors") {
+    Number n1;
+    CHECK(n1.size() == 1);
+    CHECK(n1.is_negative() == false);
+    CHECK(n1[0] == 0);
+    CHECK(n1.to_string() == "0");
 
-    REQUIRE(a.to_string() == "123456789012345678901234567890");
-    REQUIRE(b.to_string() == "123456789012345678901234567890");
+    Number n2("123");
+    CHECK(n2.size() == 3);
+    CHECK(n2.is_negative() == false);
+    CHECK((n2[0] == 3 && n2[1] == 2 &&
+           n2[2] == 1));  // TODO: add getter for digits
+    CHECK(n2.to_string() == "123");
+
+    Number n3("-123");
+    CHECK(n3.size() == 3);
+    CHECK(n3.is_negative() == true);
+    CHECK((n3[0] == 3 && n3[1] == 2 && n3[2] == 1));
+    CHECK(n3.to_string() == "-123");
+
+    Number n4(123);
+    CHECK(n4.size() == 3);
+    CHECK(n4.is_negative() == false);
+    CHECK((n4[0] == 3 && n4[1] == 2 && n4[2] == 1));
+    CHECK(n4.to_string() == "123");
+
+    Number n5(-123);
+    CHECK(n5.size() == 3);
+    CHECK(n5.is_negative() == true);
+    CHECK((n5[0] == 3 && n5[1] == 2 && n5[2] == 1));
+    CHECK(n5.to_string() == "-123");
+
+    Number n6(vector<int>{3, 2, 1});
+    CHECK(n6.size() == 3);
+    CHECK(n6.is_negative() == false);
+    CHECK((n6[0] == 3 && n6[1] == 2 && n6[2] == 1));
+    CHECK(n6.to_string() == "123");
+
+    Number n7(vector<int>{3, 2, 1}, true);
+    CHECK(n7.size() == 3);
+    CHECK(n7.is_negative() == true);
+    CHECK((n7[0] == 3 && n7[1] == 2 && n7[2] == 1));
+    CHECK(n7.to_string() == "-123");
+
+    // TODO: add tests for invalid arguments
+  }
+
+  SECTION("Set") {
+    Number n;
+    n.set("123");
+    CHECK(n.size() == 3);
+    CHECK(n.is_negative() == false);
+    CHECK((n[0] == 3 && n[1] == 2 && n[2] == 1));
+    CHECK(n.to_string() == "123");
+
+    n.set("-123");
+    CHECK(n.size() == 3);
+    CHECK(n.is_negative() == true);
+    CHECK((n[0] == 3 && n[1] == 2 && n[2] == 1));
+    CHECK(n.to_string() == "-123");
+
+    n.set("000");
+    CHECK(n.size() == 1);
+    CHECK(n.is_negative() == false);
+    CHECK(n[0] == 0);
+    CHECK(n.to_string() == "0");
+
+    n.set("-000");
+    CHECK(n.size() == 1);
+    CHECK(n.is_negative() == false);
+    CHECK(n[0] == 0);
+    CHECK(n.to_string() == "0");
   }
 
   SECTION("Multiplication") {
@@ -26,13 +92,11 @@ TEST_CASE("Numbers Methods", "[number]") {
   }
 }
 
-TEST_CASE("Numbers Benchmark", "[benchmark]") {
+TEST_CASE("Numbers Benchmark", "[!benchmark]") {
   Number a("123456789012345678901234567890");
   Number b("123456789012345678901234567890");
 
   BENCHMARK("FFT Multiplication") { return fft_multiply(a, b); };
 
   BENCHMARK("Naive Multiplication") { return column_multiply(a, b); };
-
-  // BENCHMARK("Karatsuba Multiplication") { return karatsuba_multiply(a, b); };
 }
